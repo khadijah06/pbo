0330fa287c3cd82501c47380c7b1717ef2613ae9
khadijah06 <73375517+khadijah06@users.noreply.github.com>
2020-10-24 11:09:52 +0700

    Update Shallowcopy.java

--- Shallowcopy.java
+++ Shallowcopy.java
@@ -4,13 +4,13 @@ public class Shallowcopy {
         main.whenShallowCopying_thenObjectsShouldNotBeSame();
     }
     public void whenShallowCopying_thenObjectsShouldNotBeSame() {
-        Categories categories = new Categories("bejalar java","pbo");
+        Categories categories = new Categories("bejalar java bersama ridwan","pbo");
         Product buku1 = new Product("0001", categories);
 
         Product buku2 = new Product(buku1.getIdproduct(),buku1.getcategories());
         
         buku2.setIdproduct("0002");
-        buku2.getcategories().setNamabuku("Jaringan dasar");
+        buku2.getcategories().setNamabuku("belajar Jaringan dasar bersama ridwan");
 
         System.out.println(buku1);
         System.out.println(buku2);
